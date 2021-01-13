9392e3f1c0a3c24e6a8ae5bd6f02d6e4e4c1d2ea
leovenom <leonardt@oputlook.com>
2021-01-13 19:24:38 +0100

    HashRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import {BrowserRouter as Router, Switch, Route} from  'react-router-dom'
+import {BrowserRouter as Router, Switch, Route, HashRouter } from  'react-router-dom'
 import Home from './pages';
 import SigninPage from './pages/signin';
 import Privacidade from './pages/privacidade';
@@ -7,12 +7,12 @@ import Uso from './pages/uso';
 
 function App() {
   return (
-    <Router>
+    <Router history={HashRouter}>
       <Switch>
         <Route path='/' component={Home} exact />
-        <Route path='/contact' component={SigninPage} exact/>
-        <Route path='/privacidade' component={Privacidade} exact/>
-        <Route path='/uso' component={Uso} exact />
+        <Route path='/contact' component={SigninPage}/>
+        <Route path='/privacidade' component={Privacidade} />
+        <Route path='/uso' component={Uso}/>
       </Switch>
     </Router>
   );
