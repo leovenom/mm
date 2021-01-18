cf008493eea8d779bb49efb9d29c74a5ffe42d51
leovenom <leonardt@oputlook.com>
2021-01-18 15:37:04 +0100

    fix mobile services 2

--- src/components/Services/ServiceElements.jsx
+++ src/components/Services/ServiceElements.jsx
@@ -14,7 +14,7 @@ export const ServicesContainer = styled.div`
   }
 
   @media screen and (max-width: 480px) {
-    height: 1200px;
+    height: 1300px;
   }
  `
 export const ServicesWrapper = styled.div`
